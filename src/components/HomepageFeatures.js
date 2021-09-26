85e45d80780d6c6e082f1dc1ea37d22635f12378
smickael <mickael.k.swaminathan@gmail.com>
2021-09-27 01:01:51 +0200

    Homepage details

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,31 +4,58 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
+    title: 'Why write documentation?',
+    Svg: 'img/project-setup.png',
+    description: (
+      <>
+        Developers should write documentation because it makes it easier for coworkers to use your code.
+
+      </>
+    ),
+  },
+  {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: 'img/take-it-easy.png',
     description: (
       <>
-        Test
+        W6D is a support useful for team members, easy to use. Every documentation will be gathered here and sorted by category.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Get an overview',
+    Svg: 'img/ophthalmology.png',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Allow you to get a global vision of <code>Wildcard</code> projects you are working on. Documentation is focused on the overview.
+        How are the different functions made? How is it configured?
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: 'Skill up!',
+    Svg: 'img/development-skill.png',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Documentation will help team members to get better understanding of the project. Espacially for new developers and interns.
+      </>
+    ),
+  },
+  {
+    title: 'Sharing knowledge',
+    Svg: 'img/knowledge-transfer.png',
+    description: (
+      <>
+        Well-written code is easy to read and understand. Documentation, on the other hand, is a bonus to everyone.
+      </>
+    ),
+  },
+  {
+    title: 'Markdown',
+    Svg: 'img/code-file.png',
+    description: (
+      <>
+        Markdown provides semantic meaning for content in a relatively simple way. You can write rich formatted content extremely quickly.
       </>
     ),
   },
@@ -38,7 +65,7 @@ function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <img className={styles.featureSvg} alt={title} src={Svg} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
